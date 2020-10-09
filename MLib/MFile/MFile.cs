8a374c2b94896c9793e02f0c9c6dc49c21026715
邬海欣 <whxonly@outlook.com>
2020-10-09 16:55:33 +0800

    添加项目文件。

---
+++ MLib/MFile/MFile.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MLib.MFile
+
+{
+    public class MFile
+    {
+        /// <summary>
+        /// 创建文件夹/文件
+        /// </summary>
+        /// <param name="mPath">路径</param>
+        /// <param name="mFileName">文件名</param>
+        /// <returns></returns>
+       static public async Task<bool> CreateFile(string mPath, string mFileName)
+        {
+            bool mPathR = false, mFileNameR = false;
+            string mFliePath = mPath+ mFileName;
+             return  await Task.Run(() =>
+              {
+                  if (!Directory.Exists(mPath) && mPath != "")
+                  {                     
+                      try
+                      {
+                          Directory.CreateDirectory(mPath);
+                          while (!Directory.Exists(mPath))
+                          {
+                              Task.Delay(3000);
+                              Console.WriteLine("正在创建" + mPath + "文件夹");
+                          }
+                          mPathR = true;
+                      }
+                      catch (Exception) { };
+
+                  }
+                  else
+                      mPathR = true;
+
+
+                  if (File.Exists(mFliePath) || mFliePath=="")
+                      mFileNameR= true;
+                  else
+                  {
+                      try
+                      {
+                        
+                          File.Create(mFliePath).Close();
+                          while (!File.Exists(mFliePath))
+                          {
+                              Task.Delay(3000);
+                              Console.WriteLine("正在创建" +  mFliePath + "文件");
+                          }
+
+                          mFileNameR =true;
+                      }
+                      catch (Exception e)
+                      {
+                          Console.WriteLine(e);
+                          mFileNameR= false;
+                      }
+                  }
+                  return mPathR && mFileNameR;
+              });
+        }
+
+
+    }
+}
---
+++ MLib/MyClass/MyClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MLib.MyClass
+{
+    public class MyClass
+    {
+        public static Boolean CheckStrName(string StrName)
+
+        {
+
+            StringBuilder description = new StringBuilder();
+
+            Boolean opResult = Regex.IsMatch(StrName, @"(?!((^(con)$)|^(con)\\..*|(^(prn)$)|^(prn)\\..*|(^(aux)$)|^(aux)\\..*|(^(nul)$)|^(nul)\\..*|(^(com)[1-9]$)|^(com)[1-9]\\..*|(^(lpt)[1-9]$)|^(lpt)[1-9]\\..*)|^\\s+|.*\\s$)(^[^\\\\\\/\\:\\<\\>\\*\\?\\\\\\""\\\\|]{1,255}$)");
+
+            if (!opResult)
+
+            {
+
+                description.Append("文件名包含特殊符或系统关键字！");
+
+            }
+
+
+
+            if (description.Length > 0)
+
+            {
+
+                MessageBox.Show(description.ToString());
+
+            }
+
+            return opResult;
+
+        }
+
+    }
+}
