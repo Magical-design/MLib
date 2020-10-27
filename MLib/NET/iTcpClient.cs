f5bdaff4abb446300681e86f5537abce971c53ae
邬海欣 <whxonly@outlook.com>
2020-10-27 09:00:43 +0800

    1027

---
+++ MLib/NET/iTcpClient.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace MLib.NET
+{
+    public class ITcpClient
+    {
+        public string ip = "127.0.0.1";
+        public int port = 0;
+        public TcpClient tcpClient;
+        public bool AutoConnect = false;
+        public event EventHandler<bool> ConnectStatusChange;
+        public event EventHandler<string> AutoReceiveContent;
+        private bool connect = false;
+        private bool connecting = false;
+        private bool sendAndRecLock = false;
+        private bool autoReceiving = false;
+        public bool Connect
+        {
+            get
+            {
+                return connect;
+            }
+            set
+            {
+                if (connect != value && ConnectStatusChange!=null)
+                {
+                    if(!value)
+                    {
+                        try
+                        {
+                            tcpClient.Close();
+                            
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+
+                        }
+                        tcpClient = new TcpClient();
+                    }
+                    ConnectStatusChange(null, value);
+                }
+                    
+  
+            connect = value;
+
+            }
+        }
+        public ITcpClient()
+        {
+            tcpClient = new TcpClient();
+            AutoReceive();
+        }
+        public ITcpClient(string mip, int mport)
+        {
+            ip = mip;
+            port = mport;
+            tcpClient = new TcpClient(ip, port);
+            AutoReceive();
+        }
+
+        public object lck=new object();
+        public void CheckCnt()
+        {
+
+            try
+            {
+
+           // Dispatcher.CurrentDispatcher.Invoke(new Action(() => {
+                Connect = !((tcpClient.Client.Poll(1000, SelectMode.SelectRead) && (tcpClient.Client.Available == 0)) || !tcpClient.Client.Connected);
+           // }));
+
+
+            }
+            catch (Exception ex)
+            {
+                Connect = false;
+                Console.WriteLine(ex);
+
+            }
+
+        }
+
+        public void Send(string s)
+        {
+            if (Connect)
+            {
+                try
+                {
+                    Byte[] sedData = System.Text.Encoding.ASCII.GetBytes(s);
+                    NetworkStream stream = tcpClient.GetStream();
+                    stream.Write(sedData, 0, sedData.Length);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex);
+                }
+            }
+
+
+        }
+
+        public async Task SendAsy(string s)
+        {
+            await Task.Run(() =>
+            {
+
+                Send(s);
+            });
+
+        }
+
+        public async Task<string> SendAndRecAsy(string s)
+        {
+            sendAndRecLock = true;
+            await Task.Run(()=>{ while (autoReceiving) { Thread.Sleep(50); } ; });               
+            await SendAsy(s);
+            string rs = await RecAsync();
+            sendAndRecLock = false;
+            return rs;
+
+        }
+
+
+
+        private async Task<string> RecAsync()
+        {
+            try
+            {
+
+                if (Connect)
+                {
+                    byte[] recData = new Byte[256];
+                    // String to store the response ASCII representation.
+                    String responseData = String.Empty;
+
+                    NetworkStream stream = tcpClient.GetStream();
+                    stream.ReadTimeout = 200;
+                    // Read the first batch of the TcpServer response bytes.
+                    Int32 bytes = await stream.ReadAsync(recData, 0, recData.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(recData, 0, bytes);
+                    return responseData;
+
+                }
+                else
+                    return "未连接";
+
+            }
+            catch (Exception ex)
+            {
+
+                return ex.ToString();
+            }
+        }
+
+        public async void AutoReceive()
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    try
+
+                    {
+                        CheckCnt();
+                        if (!sendAndRecLock && Connect)
+                        {
+                            autoReceiving = true;
+                            byte[] recData = new Byte[256];
+                            // String to store the response ASCII representation.
+                            String responseData = String.Empty;
+
+                            NetworkStream stream = tcpClient.GetStream();
+                            stream.ReadTimeout = 10000;
+                            // Read the first batch of the TcpServer response bytes.
+                            Int32 bytes = stream.Read(recData, 0, recData.Length);
+                            responseData = System.Text.Encoding.ASCII.GetString(recData, 0, bytes);
+                            autoReceiving = false;
+                            AutoReceiveContent?.Invoke(null, responseData);
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        autoReceiving = false;
+                        Thread.Sleep(200);
+                        Console.WriteLine(ex);
+                    }
+                    
+
+                }
+
+
+            });
+
+        }
+        public async  Task _Connect()
+        {
+            if (connecting)
+                return;
+            else
+            {
+                await Task.Run(() =>
+                {
+
+                    do
+                    {
+                        CheckCnt();
+                        if (!Connect)
+                        {
+                            try
+                            {
+                                connecting = true;
+                                tcpClient.Connect(ip, port);
+                                CheckCnt();
+                            }
+                            catch (Exception ex)
+                            {
+                                tcpClient.Close();
+                                tcpClient = new TcpClient();
+                                Console.WriteLine(ex);
+                            }
+
+                        }
+                        if (!Connect)
+                            Thread.Sleep(1500);
+                        else
+                            Thread.Sleep(200);
+
+                    } while (AutoConnect);
+                    connecting = false;
+
+
+                });
+
+            }
+
+
+        }
+
+        public void Close()
+        {
+
+            try
+            {
+                Connect = false;
+
+            }
+            catch (Exception ex)
+            {
+                tcpClient.Close();
+                tcpClient = new TcpClient();
+                Console.WriteLine(ex);
+            }
+        }
+
+    }
+}
---
+++ TcpClientSample/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using MLib.NET;
+using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+
+namespace TcpClientSample
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ITcpClient iTcpClient =new ITcpClient();
+        private bool mConnect = false;
+        public bool Connect
+        {
+            get
+            {
+                return mConnect;
+            }
+            set
+            {
+                if(mConnect!=value)
+                {
+                    if (value)
+                        btConnect.Content = "断开";
+                    else
+                        btConnect.Content = "连接";
+                }
+                mConnect = value;
+                if (mConnect)
+                {
+                    CntStu.Fill = new SolidColorBrush(Colors.Green);
+                    btSend.IsEnabled = true;
+                }
+
+
+                else
+                {
+                    CntStu.Fill = new SolidColorBrush(Colors.Red);
+                    btSend.IsEnabled = false;
+                   
+                    
+                }
+
+
+            }
+        }
+
+        public void RefreshConnect(object obj ,bool e)
+        {
+            Dispatcher.Invoke(new Action(() => { Connect = e; }));
+           
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public void Init()
+        {
+            txIP.Text = "127.0.0.1";
+            txPort.Text = "3000";
+            iTcpClient.AutoConnect = true;
+            Connect = false;
+            iTcpClient.ConnectStatusChange += RefreshConnect;
+            iTcpClient.AutoReceiveContent +=Receive;
+
+
+        }
+
+        private async void btConnect_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            
+            
+            try
+            {
+                if (!Connect)
+                {
+                    iTcpClient.ip = txIP.Text;
+                    iTcpClient.port = Int32.Parse(txPort.Text);
+                    await iTcpClient._Connect();
+
+                }
+                else
+                    iTcpClient.Close();
+
+
+            }
+            catch (System.Exception ex)
+            {
+                
+                Console.WriteLine(ex);
+            }
+            
+        }
+
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+
+        private void btSend_Click(object sender, RoutedEventArgs e)
+        {
+            iTcpClient.Send(txSendContent.Text);
+
+            //stream.Close();
+        }
+
+        public  void Receive(object obj,string s)
+        {
+            Dispatcher.Invoke(new Action(() => {
+                txRecContent.Text += s;
+
+            }));
+
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            try
+            {
+
+                iTcpClient.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
